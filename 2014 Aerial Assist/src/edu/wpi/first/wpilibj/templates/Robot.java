73f9c9845298a454567640c4caaa6baa903d7126
FRC1787 <orangerobotics@gmail.com>
2014-01-16 14:46:26 -0500

    Numbered Jaguars

--- 2014 Aerial Assist/src/edu/wpi/first/wpilibj/templates/Robot.java
+++ 2014 Aerial Assist/src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -4,11 +4,9 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
-
-import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,24 +16,39 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class Robot extends SimpleRobot {
+
+    /*
+     * Jaquar Motor Controllers
+     */
+    Jaguar leftMotor1;
+    Jaguar leftMotor2;
+    Jaguar rightMotor1;
+    Jaguar rightMotor2;
+    Jaguar pickupMotor;
+
+    /*
+     * pnuematics
+     */
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
+        leftMotor1 = new Jaguar(2);
+        leftMotor2 = new Jaguar(3);
+        rightMotor1 = new Jaguar(4);
+        rightMotor2 = new Jaguar(5);
+        pickupMotor = new Jaguar(6);
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
     }
-    
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
     }
 }
